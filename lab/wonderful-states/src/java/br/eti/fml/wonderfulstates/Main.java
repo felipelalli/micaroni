ec883d6d867c6e5dd3cd07d69b591314e63bd174
Felipe Micaroni Lalli <micaroni@gmail.com>
2010-03-21 19:43:58 -0300

    first hello world!

---
+++ lab/wonderful-states/src/java/br/eti/fml/wonderfulstates/Main.java
@@ -0,0 +1,12 @@
+package br.eti.fml.wonderfulstates;
+
+/**
+ *
+ * @author felipelalli
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("hello world!");
+    }
+}
