00079ba1e5c17cd83f2d2b4c57b514972910c09f
Felipe Micaroni Lalli <micaroni@gmail.com>
2011-03-30 23:36:58 -0300

    new project crazydb

---
+++ lab/sandbox/crazydb/src/br/eti/fml/crazydb/Test.java
@@ -0,0 +1,10 @@
+package br.eti.fml.crazydb;
+
+/**
+ * @author Felipe Micaroni Lalli (felipe.micaroni@movile.com / micaroni@gmail.com)
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ lab/sandbox/crazydb/src/br/eti/fml/data/db/keyvalue/KeyValueImpl.java
@@ -0,0 +1,107 @@
+package br.eti.fml.data.db.keyvalue;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
+/**
+ * A basic implementation helper.
+ *
+ * @author Felipe Micaroni Lalli (felipe.micaroni@movile.com / micaroni@gmail.com)
+ */
+public abstract class KeyValueImpl implements KeyValue {
+    private final Map<String, Queue<Operation>> operations
+            = new HashMap<String, Queue<Operation>>();
+
+    protected enum OperationType {
+        INSERT, DELETE
+    }
+
+    protected class Operation {
+        private final OperationType type;
+        private final String key;
+        private final String column;
+        private final byte[] value;
+
+        Operation(OperationType type, String key, String column, byte[] value) {
+            this.type = type;
+            this.key = key;
+            this.column = column;
+            this.value = value;
+        }
+
+        public OperationType getType() {
+            return type;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public byte[] getValue() {
+            return value;
+        }
+
+        public String getColumn() {
+            return column;
+        }
+    }
+
+    protected Queue<Operation> getQueueOperations(String queueId) {
+        return this.operations.get(queueId);
+    }
+
+    @Override
+    public void lazyPut(String queueId, String key, byte[] data) throws IOException {
+        this.lazyPut(queueId, key, KeyValue.DEFAULT_COLUMN, data);
+    }
+
+    @Override
+    public void lazyPut(String queueId, String key, String column, byte[] data) throws IOException {
+        if (!operations.containsKey(queueId)) {
+            operations.put(queueId, new LinkedList<Operation>());
+        }
+
+        Operation operation = new Operation(OperationType.INSERT, key, column, data);
+        operations.get(queueId).offer(operation);
+    }
+
+    @Override
+    public void lazyDelete(String queueId, String key) throws IOException {
+        this.lazyDelete(queueId, key, KeyValue.DEFAULT_COLUMN);
+    }
+
+    @Override
+    public void lazyDelete(String queueId, String key, String column) throws IOException {
+        if (!operations.containsKey(queueId)) {
+            operations.put(queueId, new LinkedList<Operation>());
+        }
+
+        Operation operation = new Operation(OperationType.DELETE, key, null, null);
+        operations.get(queueId).offer(operation);
+    }
+
+    @Override
+    public byte[][] getSlice(String key, String startColumn, String endColumn)
+            throws IOException, UnsupportedOperationException {
+
+        throw new UnsupportedOperationException("getSlice is not supported yet!");
+    }
+
+    @Override
+    public byte[] get(String key) throws IOException {
+        return this.get(key, KeyValue.DEFAULT_COLUMN);
+    }
+
+    @Override
+    public void put(String key, byte[] data) throws IOException {
+        this.put(key, KeyValue.DEFAULT_COLUMN, data);
+    }
+
+    @Override
+    public void delete(String key) throws IOException {
+        this.delete(key, KeyValue.DEFAULT_COLUMN);
+    }
+}
