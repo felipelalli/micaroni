76991f93be156ebb346ac21ef903d8bd4ffe6535
Felipe Micaroni Lalli <micaroni@gmail.com>
2011-10-23 05:52:03 -0200

    vcvcvcvc quer...

---
+++ lab/sandbox/vcquer.eu/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Vc vc vc quer?</title>
+<meta charset="utf-8" />
+</head>
+
+<body style="text-align:center;">
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=264086123628247";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<h1>Você você você quer?</h1>
+
+<audio id="quer" autoplay="autoplay" loop="loop">
+  <source src="vcquer<?php echo rand(1, 3);  ?>.mp3" type="audio/mpeg" />
+
+</audio>
+
+<p>
+<img src="vcquer1.gif"/>
+</p>
+
+<a id="link" href="javascript:document.getElementById('quer').volume=0;document.getElementById('link').style.visibility='hidden'">Para a música agora!</a>
+
+<p>
+<div class="fb-like" data-href="http://vcquer.eu" data-action="eu quero" data-send="true" data-width="600" data-show-faces="true"></div>
+</p>
+<p>
+<div class="fb-comments" data-href="http://vcquer.eu" data-num-posts="10" data-width="600"></div>
+</p>
+
+</body>
+
+</html>
