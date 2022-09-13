0713801fefb48c4e1480c96fb87324111a9544b5
Felipe Micaroni Lalli <micaroni@gmail.com>
2022-09-13 19:22:56 -0300

    Removing Elpa packages from git.

--- emacs-2022/.emacs.d/elpa/recentf-ext-20170926.35/recentf-ext-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from recentf-ext-20170926.35/recentf-ext.el  -*- no-byte-compile: t -*-
-(define-package "recentf-ext" "20170926.35" "Recentf extensions" 'nil :commit "450de5f8544ed6414e88d4924d7daa5caa55b7fe" :authors '(("rubikitch" . "rubikitch@ruby-lang.org")) :maintainer '("rubikitch" . "rubikitch@ruby-lang.org") :keywords '("convenience" "files") :url "http://www.emacswiki.org/cgi-bin/wiki/download/recentf-ext.el")
